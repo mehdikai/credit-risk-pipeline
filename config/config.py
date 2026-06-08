f360e255b3447e7eb355532aed9ebcb96c47dc0a
Mehdi <elkaissounielmehdi@gmail.com>
2026-06-08 15:37:50 +0100

    init: project structure and config

---
+++ config/config.py
@@ -0,0 +1,22 @@
+# config/config.py
+STORAGE_MODE = "local"  # switch to "minio" later
+
+PATHS = {
+    "local": {
+        "bronze": "warehouse/bronze/",
+        "silver": "warehouse/silver/",
+        "gold":   "warehouse/gold/",
+    },
+    "minio": {
+        "bronze": "s3://bronze/",
+        "silver": "s3://silver/",
+        "gold":   "s3://gold/",
+    }
+}
+
+BRONZE_PATH = PATHS[STORAGE_MODE]["bronze"]
+SILVER_PATH = PATHS[STORAGE_MODE]["silver"]
+GOLD_PATH   = PATHS[STORAGE_MODE]["gold"]
+
+DQ_REPORT_PATH = "dq_checks/reports/"
+MLFLOW_URI     = "mlflow/"
